1cf32ebccb80d2abb334c28cd9cabd8879dae12a
inka17 <innksang1007@gmail.com>
2025-12-30 03:33:43 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,26 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL || begin > end) return false;
+
+
+    int n = (int)(end - begin);
+    if (n <= 1) return true;
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (begin[j].key > begin[j + 1].key) {
+                item tmp = begin[j];
+                begin[j] = begin[j + 1];
+                begin[j + 1] = tmp;
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+    }
+
+    return true;
+
 }
